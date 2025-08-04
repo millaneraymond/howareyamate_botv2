a076b76b977f80d82679d453f69754d59a25dc05
millaneraymond <millaneraymond20@gmail.com>
2025-08-04 20:51:10 +1000

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { nanoid } from '@/lib/utils'
-import { Chat } from '@/components/chat'
+import { redirect } from 'next/navigation';
 
-export const runtime = 'edge'
-
-export default function IndexPage() {
-  const id = nanoid()
-
-  return <Chat id={id} />
+export default function Home() {
+  redirect('/chat');
 }
